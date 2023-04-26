261205ec7c4bd8c4406fd70175648e646f3abc14
Akash Patel <akash.patel2520@gmail.com>
2023-04-25 20:58:38 -0400

    Add files via upload
    Initial Commit

---
+++ SwiftUI-TicTacToe/SwiftUI-TicTacToe/SwiftUI_TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TicTacToeApp.swift
+//  SwiftUI-TicTacToe
+//
+//  Created by Akash on 2023-04-25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
